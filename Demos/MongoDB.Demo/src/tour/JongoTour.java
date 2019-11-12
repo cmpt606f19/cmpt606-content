9bdc32dc304126c5682b4f2d36eaa25d1b060316
ae <erradi@live.com>
2019-11-12 08:58:45 +0300

    Update JongoTour.java

--- Demos/MongoDB.Demo/src/tour/JongoTour.java
+++ Demos/MongoDB.Demo/src/tour/JongoTour.java
@@ -1,21 +1,16 @@
 package tour;
 
-import static com.mongodb.client.model.Filters.eq;
-import static java.util.Arrays.asList;
-
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
 import org.jongo.MongoCursor;
-import org.jongo.query.Query;
 
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.WriteResult;
 
+//More info and examples @ https://jongo.org/
 public class JongoTour {
 
 	public static void main(String[] args) {
@@ -24,10 +19,11 @@ public class JongoTour {
 	    
 	    MongoCollection friendsCol = jongo.getCollection("friends");
 		
-		Query query = jongo.createQuery("{test:1}");
+	    //Drop all documents
+		friendsCol.drop();
 		
         // make a document and insert it
-        Friend ali = new Friend("Ali Faleh", 20, "Female", new Address("University St", "Doha"));
+        Friend ali = new Friend("Ali Faleh", 20, "Male", new Address("University St", "Doha"));
         System.out.printf("Friend object before insert: %s%n", ali);
         
         friendsCol.insert(ali);
@@ -36,37 +32,38 @@ public class JongoTour {
         System.out.printf("Friend object after insert: %s%n", ali);
         
         // Now, lets add some more friends so we can explore queries and cursors
-        List<Friend> myFriends = asList(
+        Friend[] myFriends = {
                 new Friend("Abbess Ibn Firnas", 45, "Male", new Address("5 Arab League Street", "Al-Khor")),
                 new Friend("Khadija Khaldi", 28, "Female", new Address("55 Pearl Rd", "Doha")),
-                new Friend("Samir Dahih", 61, "Male", new Address("444 Corniche St", "Dukhan"))
-        );
+                new Friend("Samira Dahih", 18, "Female", new Address("444 Corniche St", "Dukhan"))
+        };
         
-        for(var friend : myFriends) {
-        	friendsCol.insert(friend);
-        }
+        //Insert multiple documents in one go...
+        friendsCol.insert(myFriends);
         System.out.println("Total # of friends " + friendsCol.count());
 
         System.out.println("\nAll friends in the collection");
-        MongoCursor<Friend> friends = friendsCol.find().as(Friend.class);
+        MongoCursor<Friend> friends = friendsCol.find()
+        										.sort("{name: 1}")
+        										.as(Friend.class);
 		for(var friend : friends) {
 			System.out.println(friend);
 		}
 
 		System.out.println("\nFriends in the collection with the name contains Khaldi (/i for case-insensitive) & age >= 20");
-		String queryString = "{name: { $regex : # }, age: {$gte: #}}";
-		friends = friendsCol.find(queryString, "khaldi", 20).as(Friend.class); 
+		String query = "{name: { $regex : # }, age: {$gte: #}}";
+		friends = friendsCol.find(query, "khaldi", 20).as(Friend.class); 
 		for(var friend : friends) {
 			System.out.println(friend);
 		}
 		
 		System.out.println("\nFirst friend in the collection with the name contains Khaldi (/i for case-insensitive) & age >= 20");
-		Friend one = friendsCol.findOne(queryString, "khaldi", 20).projection("{_id: 0, name: 1, age: 1}").as(Friend.class);
+		Friend one = friendsCol.findOne(query, "khaldi", 20).projection("{_id: 0, name: 1, age: 1}").as(Friend.class);
 		System.out.println(one);
 		
         System.out.println("\nFriends living in Doha");
         // now use a query to get 1 document out { address.city:  "Doha" }
-        friends = friendsCol.find("{address.city: 'Doha'}").as(Friend.class);
+        friends = friendsCol.find("{address.city: #}", "Doha").as(Friend.class);
 		for(var friend : friends) {
 			System.out.println(friend);
 		}
@@ -75,8 +72,8 @@ public class JongoTour {
 		List<String> names = friendsCol.distinct("name").as(String.class);
 		System.out.println(names);
 		
-		System.out.println("\nFriends older than 40");
-		var results = friendsCol.aggregate("{$match: {age: {$gte: 40 }} }")
+		System.out.println("\nFriends older than 30");
+		var results = friendsCol.aggregate("{$match: {age: {$gte: # }} }", 30)
 					        	.and("{$limit:2}")
 					        	.as(Friend.class);
 		
@@ -104,7 +101,47 @@ public class JongoTour {
 			System.out.println(result);
 		}
 		
-		friendsCol.drop();
+		
+        System.out.println("\nUpdate Ali's name and age");
+        friendsCol.update("{name: #}", "Ali Faleh").with("{$set: {age: #, name: #}}", 25, "Dr. Ali Faleh");
+		one = friendsCol.findOne("{name: #}", "Dr. Ali Faleh").as(Friend.class);
+		System.out.println(one);
+		
+        System.out.println("\nUpdate Ali's address");
+        WriteResult writeResult = friendsCol.update("{name: #}", "Dr. Ali Faleh").with("{$set: {address: #}}", new Address("55 Porto Arabia, The Pearl,", "Doha"));
+        System.out.println(writeResult);
+        one = friendsCol.findOne("{name: #}", "Dr. Ali Faleh").as(Friend.class);
+		System.out.println(one);
+		
+		
+        System.out.println("\nUpset all my female friends by increasing their age by 5 😃");
+        writeResult = friendsCol.update("{gender: #}", "Female").multi().with("{$inc: {age: #}}", 5);
+        System.out.println(writeResult);
+		friends = friendsCol.find("{gender: #}", "Female").as(Friend.class);
+		for(var friend : friends) {
+			System.out.println(friend);
+		}
+		
+		System.out.println("\nUpdate a document and it does not exist create it"); 
+		writeResult = friendsCol.update("{name: #}", "Shaun the Sheep").upsert().with("{$inc: {age: 1}}");
+        System.out.println(writeResult);
+        one = friendsCol.findOne("{name: #}", "Shaun the Sheep").as(Friend.class);
+		System.out.println(one);
+		
+		System.out.println("\nReplace a document"); 
+		writeResult = friendsCol.update("{name: #}", "Shaun the Sheep").with(new Friend("Shaun the Sheep", 3, "Sheep", new Address("Fun St", "Utopia")));
+        System.out.println(writeResult);
+        one = friendsCol.findOne("{name: #}", "Shaun the Sheep").as(Friend.class);
+		System.out.println(one);
+		
+		System.out.println("\nRemove a document"); 
+		writeResult = friendsCol.remove("{name: #}", one.getName());
+		System.out.println(writeResult);
+		writeResult = friendsCol.remove(new ObjectId(one.getId()));
+		System.out.println(writeResult);
+		
+        // release resources
+        mongoClient.close();
 	}
 
 }
